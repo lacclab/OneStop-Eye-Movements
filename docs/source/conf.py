474845fa47480900d5f4cfe7098c0b6ac4342207
Omer Shubi <omer.shubi@gmail.com>
2024-03-15 16:47:14 +0200

    readme strucure WIP

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,14 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "OneStop-Eye-Tracking"
-copyright = "2024, LaCC Lab"
-author = "??"
-
-# The full version, including alpha/beta/rc tags
+copyright = "2024, TBD"
+author = "TBD"
 release = "1.0"
 
 
