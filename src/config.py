ba491853cf667de290c47d54296df46a68c30fd7
ellalion <ell.ella.lion@gmail.com>
2025-01-01 16:16:43 +0200

    recalculation of duration using ia report + finding missing ia_a trails 'l55_519', 2, 9, 2, 0 ; 'l56_489', 1, 10, 6, 0

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_PATH = Path("/data/home/shared/onestop/new")
+BASE_PATH = Path("../data")
 
 METADATA_PATH = Path(BASE_PATH, "Metadata OneStopGaze L1 - metadata.csv")
 FULL_REPORT_PATH = Path(BASE_PATH, "full_report.csv")
@@ -10,12 +10,12 @@ QUESTIONNAIRE_PATH = Path(BASE_PATH, "questionnaire.json")
 SESSION_SUMMARY_PATH = Path(BASE_PATH, "session_summary.csv")
 
 REPORTS_BASE_PATH = Path(BASE_PATH, "Outputs")
-IA_P_PATH = Path(REPORTS_BASE_PATH, "IA reports/ia_P.csv")
-IA_Q_PATH = Path(REPORTS_BASE_PATH, "IA reports/ia_Q.tsv")
-IA_A_PATH = Path(REPORTS_BASE_PATH, "IA reports/ia_A.tsv")
-IA_T_PATH = Path(REPORTS_BASE_PATH, "IA reports/ia_T.tsv")
-IA_QA_PATH = Path(REPORTS_BASE_PATH, "IA reports/ia_QA.tsv")
-IA_Q_preview_PATH = Path(REPORTS_BASE_PATH, "IA reports/ia_Q_preview.tsv")
+IA_P_PATH = Path(REPORTS_BASE_PATH, "raw_ia_reports/ia_P.tsv")
+IA_Q_PATH = Path(REPORTS_BASE_PATH, "raw_ia_reports/ia_Q.tsv")
+IA_A_PATH = Path(REPORTS_BASE_PATH, "raw_ia_reports/ia_A.tsv")
+IA_T_PATH = Path(REPORTS_BASE_PATH, "raw_ia_reports/ia_T.tsv")
+IA_QA_PATH = Path(REPORTS_BASE_PATH, "raw_ia_reports/ia_QA.tsv")
+IA_Q_preview_PATH = Path(REPORTS_BASE_PATH, "raw_ia_reports/ia_Q_preview.tsv")
 TRIAL_PATH = Path(REPORTS_BASE_PATH, "trial_report.tsv")
 #TRIAL_P_PATH = Path(REPORTS_BASE_PATH, "trial reports/trial_P.tsv")
 #TRIAL_QA_PATH = Path(REPORTS_BASE_PATH, "trial reports/trial_QA.tsv")
