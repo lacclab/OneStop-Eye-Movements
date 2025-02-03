b036242956b70e431f9ed7af99c3f8da503f04a9
meiri.yoav (laccl-srv1) <meiri.yoav@campus.technion.ac.il>
2025-02-03 16:05:51 +0200

    Add classifiers to setup.py for linear_mm_utils package

--- src/linear_mm_utils/setup.py
+++ src/linear_mm_utils/setup.py
@@ -6,6 +6,16 @@ setup(
     description=(
         "Julia and Python tools for linear mixed models"
     ),
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        "Programming Language :: Python :: 3",
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        "License :: GPLv3",
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     packages=find_packages(where="linear_mm_utils"),
     install_requires=[
         # add juliacall
